7deaf4af65ae554f6fc2e9427f435ce549adc627
Sharan Jayakumar <sharan.jayakumar@mallow-tech.com>
2025-08-16 17:36:49 +0530

    fastify adapter config for nest db server

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -17,9 +16,25 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    return request(app.getHttpServer()).get('/').expect(200).expect({
+      message: 'Hello World!',
+      status: 'success',
+    });
+  });
+
+  it('/calculate-date (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/calculate-date?date=01-01-2000')
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body).toHaveProperty('years');
+        expect(res.body).toHaveProperty('months');
+        expect(res.body).toHaveProperty('days');
+        expect(res.body).toHaveProperty('totalDays');
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
